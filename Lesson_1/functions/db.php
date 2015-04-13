70dec7cc9994a5f0d2ad09bbcf0bccc1af8f2248
4udesenko <nikita@joylia.com>
2015-04-13 16:33:31 +0300

    add function addNews

--- Lesson_1/functions/db.php
+++ Lesson_1/functions/db.php
@@ -22,3 +22,11 @@ function dbFindOneByQuery($sql)
     return dbFindAllByQuery($sql)[0];
 }
 
+function addNews($title, $text)
+{
+    dbConnect();
+    $title = mysql_real_escape_string($title);
+    $text = mysql_real_escape_string($text);
+    $query = "INSERT INTO news (title, text, date) VALUES ('$title', '$text', NOW())";
+    return mysql_query($query);
+}
