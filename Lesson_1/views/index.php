366588eeee3c12ee52ca6f3e1021419a2dd7dd2c
4udesenko <nikita@joylia.com>
2015-04-16 00:14:54 +0300

    add commit

---
+++ Lesson_1/views/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<h1>Новости</h1>
+
+<div><a href="/Lesson_1/form.php">
+        <button>Добавить новость</button>
+    </a></div>
+<?php foreach ($news as $article) : ?>
+    <article>
+        <h3>
+            <a href="/Lesson_1/article.php?id=<?php echo $article['id']; ?>">
+                <?php echo $article['title']; ?>
+            </a>
+        </h3>
+
+        <div><?php echo $article['text']; ?></div>
+    </article>
+<?php endforeach; ?>
+</body>
+</html>
