e5fc35314ccbc86c6b2bf512bb012b5e6059903b
4udesenko <nikita@joylia.com>
2015-04-16 15:06:02 +0300

    Delete article.php

--- Lesson_3/article.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$ctrl = !empty($_GET['ctrl']) ? $_GET['ctrl'] : 'news';
-$ctrlClassName = ucfirst($ctrl) . 'Controller';
-
-require __DIR__ . '/controllers/' . $ctrlClassName . '.php';
-
-$controller = new $ctrlClassName;
-$controller->actionOne($_GET['id']);
